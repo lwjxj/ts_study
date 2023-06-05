a7df0bd934cd42c448f330310836eb70b81fb79b
liuwei <1097408921@qq.com>
2023-06-05 16:06:43 +0800

    first commit

---
+++ src/components/01_dataType.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    // js类型  null undefined string number boolean bigint symbol object(function array Date Math...)
+    // ts类型  在js类型的基础上，再增加void any never unknown enum 自定义类型（type interface）
+</script>
---
+++ src/components/02_类型签名&Record.vue
@@ -0,0 +1,7 @@
+<script lang="ts">
+type A = Object
+const a:A = () => 1
+
+const b:Array<number> = [1]
+const c:Function = () => 1
+</script>
