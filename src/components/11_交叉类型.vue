09cc9cd557612c7209dd1666a0b61c21fddfe4fc
liuwei <1097408921@qq.com>
2023-06-20 22:14:23 +0800

    feat: 交叉类型

--- src/components/11_交叉类型.vue
+++ src/components/11_交叉类型.vue
@@ -3,14 +3,14 @@ type A100 = string & number
 
 
 type Person100 = {
-    id:string
+    id: string
 }
 
 type User = {
     id: number
 } & Person100
 
-const a100:User = {
+const a100: User = {
     id: 100
 }
 </script>
